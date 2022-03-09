7b1e74f31b6a4ac937faa7976f8176cee1167d0b
meriem <meriem.mghirbi@esprit.tn>
2022-03-09 21:54:08 +0100

    ajout

---
+++ ennemi.h
@@ -0,0 +1,9 @@
+#include "fonction.h"
+void initEnnemi(Ennemi*e)
+void afficherEnnemi(Ennemi e, SDL_Surface * screen)
+void animerEnnemi( Ennemi * e)
+void deplacer( Ennemi * e)
+int collisionBB( personne p, Ennemi e) ou int collisionBB( SDL_Rect posp, SDL_Rect pose)
+void deplacerIA( Ennemi * e, SDL_Rect posPerso)
+#endif
+
